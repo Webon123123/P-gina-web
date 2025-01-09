09c83ec3e64fc5f154a275848f94db9c46c965a3
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2025-01-09 15:40:48 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
+  JWT_SECRET: process.env.JWT_SECRET || "keyapimashaaaaaaaa",
   RECAPTCHA_SECRET: process.env.RECAPTCHA_SECRET,
 };
