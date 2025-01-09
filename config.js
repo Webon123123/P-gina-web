b32fe4e995441ee93700b18b2e9fdbfc34755197
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2025-01-09 15:42:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET || "keyapimashaaaaaaaa",
+  JWT_SECRET: process.env.JWT_SECRET || "supersecreykey",
   RECAPTCHA_SECRET: process.env.RECAPTCHA_SECRET,
 };
